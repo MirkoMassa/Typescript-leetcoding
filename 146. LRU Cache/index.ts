9e121bf46aaaba3bf17f85588f906d1749c84832
Mirko <mirkomassa18@gmail.com>
2022-11-21 13:24:02 +0100

    LRU Cache completed with hash table and array

--- 146. LRU Cache/index.ts
+++ 146. LRU Cache/index.ts
@@ -1,120 +1,63 @@
 //nodes are organized as a double linked list for the insertion,
 //and as an hash map for the get method (key pointing to the value of the node)
 
-export class Node{
-    public prev: Node | null;
-    public next: Node | null;
-    constructor(public key:number, public value:number){
-        this.key = key;
-        this.value = value;
-
-        this.prev = this.next = null;
-
-    }
-    setPair(key:number, value:number){
-        this.key = key;
-        this.value = value;
-    }
-}
+//***Tried using an array***
 
 //need to keep track of the capacity of the cache
 export class LRUCache{
     public cache: Map<number, number>;
-    public lr: Node; //least recently used node
-    public mr: Node; //most recently used node
-    public size: number;
+    public nodes: number[];
 
     constructor(public capacity: number){
         this.capacity = capacity;
-        this.size = 0;
         this.cache = new Map();
 
-        this.lr = this.mr = new Node(-1, 0);
-        this.lr.next = this.mr;
-        this.mr.prev = this.lr;
+        this.nodes = new Array();
 
     }
     
     put(key: number, value: number): void{
 
-        //0 elements in the cache: lru and mru will point to new key
-        if(this.mr.key === -1){
+        //0 elements in the cache
+        if(this.nodes[0] === undefined){
 
-            this.lr.setPair(key, value);
-            this.mr.setPair(key, value);
-
+            this.nodes[0] = key;
             this.cache.set(key, value);
-            this.size++;
             return;
         }  
         
-        //***elements in the cache: resetting mru and lru***//
+        //***elements in the cache***//
 
         //existing key: no size increasing, resetting mru
         if(this.cache.has(key)){
 
-            //looking for the key's actual position
-            let oldnode: Node = this.lr;
-            
-            while(oldnode.key != key){
-                //loop will break before finding null
-                oldnode = oldnode.next!;
+            //removing the key from the array and putting it at the end
+            let i = 0;
+            while(this.nodes[i] != key){
+                i++;
             }
-            //various position cases
-            if(oldnode === this.lr){
-                oldnode.next!.prev = null;
-            }
-            else if(oldnode === this.mr){
-                oldnode.prev!.next = null;
-            }
-            else{
-                oldnode.prev!.next = oldnode.next;
-                oldnode.next!.prev = oldnode.prev;
-            }
-            
-            this.mr.prev = this.mr;
-            this.mr.setPair(key, value);
+            this.nodes.splice(i, 1);
+            this.nodes.push(key);
             
             this.cache.set(key, value);
             return;
         }
 
         //cache already full
-        if(this.size = this.capacity){
+        if(this.nodes.length === this.capacity){
 
-            this.cache.delete(this.lr.key);
-            this.lr = this.lr.next!;
-
-
-            this.mr.prev = this.mr;
+            this.cache.delete(this.nodes[0]);
+            this.nodes.shift();
 
             this.cache.set(key, value);
+            this.nodes.push(key);
             return;
         }
 
-        //capacity 4
-        //put 3
-        // 3
-        //put 2
-        // 3 - 2
-        //put 5
-        // 3 - 2 - 5
-        //put 7
-        // 3 - 2 - 5 - 7
-
-
         //cache still not full
         else{
-            if(this.size >=2){
-                //current most recent
-                const currmr = this.mr;
-                currmr.next = new Node(key, value);
-                this.mr = currmr.next;
-                this.mr.prev = currmr;
-            }
-            else  this.mr.setPair(key, value);
-
-            this.size++;
+            
+            this.nodes.push(key);
             this.cache.set(key, value);
             return;
         }
@@ -124,30 +67,14 @@ export class LRUCache{
     get(key: number): number{
 
         if(this.cache.has(key)){
-            //update value to most recently used
 
-            this.lr = this.lr.next!;
-
-            this.mr.setPair(key, this.cache.get(key)!);
-
-
-            let oldnode: Node = this.lr;
-            
-            while(oldnode.key != key){
- 
-                oldnode = oldnode.next!;
+            let i = 0;
+            while(this.nodes[i] != key){
+                i++;
             }
 
-            if(oldnode === this.lr){
-                oldnode.next!.prev = null;
-            }
-            else if(oldnode === this.mr){
-                oldnode.prev!.next = null;
-            }
-            else{
-                oldnode.prev!.next = oldnode.next;
-                oldnode.next!.prev = oldnode.prev;
-            }
+            this.nodes.splice(i, 1);
+            this.nodes.push(key);
 
             return this.cache.get(key)!;
         }
--- 146. LRU Cache/tests.ts
+++ 146. LRU Cache/tests.ts
@@ -1,4 +1,4 @@
-import {LRUCache, Node} from './index'
+import {LRUCache} from './index'
 import assert from 'assert'
 
 describe('lru cache', ()=>{
@@ -20,19 +20,21 @@ describe('lru cache', ()=>{
             console.log("put 1")
             cache.put(1, 1) // 2 - 6 - 4 - 1 
             console.log("put 1")
-
-            assert.equal(cache.size, 4);
-            assert([1,2,6,4].every(x => cache.cache.has(x)), "Cache does not have all values added so far");   
-
-            cache.put(5, 3) // 6 - 4 - 1 - 5 - 0
-
+            console.log("expected: 2-6-4-1")
             console.log(cache)
-            cache.put(7, 1) // 4 - 1 - 5 - 7 - 0
 
+            assert([1,2,6,4].every(x => cache.cache.has(x)), "Cache does not have all values added so far");   
             
-            console.log("lr: ",cache.lr)
-            console.log("lr next: ",cache.lr.next)
-            console.log(cache.mr.prev!.prev)
+            cache.put(5, 3) // 6 - 4 - 1 - 5
+            console.log("put 5")
+            console.log("expected: 6-4-1-5")
+            console.log(cache)
+            
+            cache.put(7, 1) // 4 - 1 - 5 - 7
+            console.log("put 7")
+            console.log("expected: 4-1-5-7")
+            console.log(cache)
+            
             assert([5, 7, 1, 4].every(x => cache.cache.has(x)), "Cache does not have all values added so far");
             
         })
